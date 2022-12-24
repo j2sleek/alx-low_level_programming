861e67795c01a192934b8ffa974b7c1c028bf9af
Teite Sampson <j2sleek@gmail.com>
2022-12-24 02:34:59 +0000

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, z;
+	a = 97;
+	z = 122;
+
+	while (a <= z)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
