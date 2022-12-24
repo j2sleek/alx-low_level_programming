32e1a347bd8c995cb5a3eaab9061980956819b54
Teite Sampson <j2sleek@gmail.com>
2022-12-24 02:49:20 +0000

    made changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@ int main(void)
 	int lower, upper;
 
 	lower = 97;
-	upper = 90;
+	upper = 65;
 
 	while (lower <= 122)
 	{
 		putchar(lower);
 		lower++;
 	}
-	while (upper <= 120)
+	while (upper <= 90)
 	{
 		putchar(upper);
 		upper++;
