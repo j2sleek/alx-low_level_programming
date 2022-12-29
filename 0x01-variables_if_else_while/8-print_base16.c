8315fc54801f69f5bea831350e8c9080808879e8
Teite Sampson <j2sleek@gmail.com>
2022-12-29 07:21:59 +0000

    added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j <= 102; j++)
+	{
+		putchar(j);
+	}
+	putchar(10);
+	return (0);
+}
