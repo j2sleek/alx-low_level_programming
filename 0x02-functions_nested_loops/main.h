f557588289620f708a368672086b2bab58e4e31d
Teite Sampson <j2sleek@gmail.com>
2023-01-01 21:31:22 +0000

    made changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,10 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
 /**
  * _putchar - writes the character c to stdout
  *
  * Return: On success, 1
  */
 int _putchar(char c);
+
+#endif
