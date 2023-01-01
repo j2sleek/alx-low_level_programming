c04d7900b408b77f162beeb7f2748e919c50faf6
Teite Sampson <j2sleek@gmail.com>
2023-01-01 21:58:49 +0000

    added 1-main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,18 +1,13 @@
 #include "main.h"
 /**
- * main - function that prints the alphabet in lowercase followed by a new line
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
-	char i = 'a';
+	char i;
+	i = 'a';
 
 	while(i <= 'z')
 	{
@@ -20,5 +15,4 @@ void print_alphabet(void)
 		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
