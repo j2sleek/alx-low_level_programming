d984351bf03e6592301f502c5f5d2e9b2da5f8c2
Teite Sampson <j2sleek@gmail.com>
2023-01-04 09:23:14 +0000

    made changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,28 +5,16 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, l, m;
+	int i, j;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i < 24; i++)
 	{
-		if (i == 2)
-			m = j <= 3;
-		else
-			m = j <=9;
-		for (j = 0; m; j++)
+		for (j = 0; j < 60; j++)
 		{
-		for (k = 0; k <= 5; k++)
-		{
-		for (l = 0; l <= 9; l++)
-		{
-		_putchar(i);
-	_putchar(j);
-_putchar(':');
-_putchar(k);
-_putchar(l);
-_putchar('\n');
-		}
-		}
+			_putchar('0' + i);
+			_putchar(':');
+			_putchar('0' + j);
+			_putchar('\n');
 		}
 	}
 }
